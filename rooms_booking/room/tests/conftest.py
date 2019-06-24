a92889ae987145079fbb8a10cd13ae4da52346af
Pierre Verkest <pverkest@anybox.fr>
2019-06-24 23:51:55 +0200

    fix fixture name

--- rooms_booking/room/tests/conftest.py
+++ rooms_booking/room/tests/conftest.py
@@ -3,8 +3,8 @@ from anyblok.conftest import *  # noqa: F401,F403
 
 
 @pytest.fixture()
-def an_address(rolback_registry):
-    return rolback_registry.Address.insert(
+def an_address(rollback_registry):
+    return rollback_registry.Address.insert(
         first_name="The Queen's College",
         last_name="University of oxford",
         street1="High Street",
